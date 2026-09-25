92c0c1611780524d804f4b33ea0bed2d370c7b89
Cyberclutch146 <blazingswagata@gmail.com>
2026-09-25 19:18:52 +0530

    changed to openstreetmaps

--- src/components/map/RiskMap.tsx
+++ src/components/map/RiskMap.tsx
@@ -123,8 +123,8 @@ export default function RiskMap({
         attributionControl={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {data && data.features.length > 0 && (
           <RiskOverlay collection={data} activeLayer={activeLayer} />
